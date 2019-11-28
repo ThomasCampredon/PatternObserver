3fc3c252c25e02454779fcb8f730d77d6416af58
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2019-11-28 13:54:04 +0100

    initialisation du code du téléphone

---
+++ codeTéléphone/telephone/src/main/java/fr/unilim/iut/telephone/App.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.telephone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
